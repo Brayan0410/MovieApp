7e767152b636220e3e1efd1b90d14c902e741bc0
Luis Gutierrez <luisgutierrez@Luiss-MacBook-Pro.local>
2024-05-21 23:09:52 -0600

    Create loginview RegisterView

--- MovieApp/Moduls/LoginView.swift
+++ MovieApp/Moduls/LoginView.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct LoginView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
 
+@StateObject var viewModel = LoginViewViewModel()
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                (title: "MovieApp",subtitle: "Get things  done",angle: 15, background: .pink)
+            
+                //login form
+                Form {
+                    if !viewModel.errorMessage.isEmpty {
+                        Text(viewModel.errorMessage)
+                            .foregroundColor(Color.red)
+                    }
+                    
+                    TextField("Email Address", text: $viewModel.email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
+                    
+                    SecureField("Password", text: $viewModel.password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    }
+                    .padding()
+                }
+                .offset(y: -50)
+                
+                //Create Account
+                
+                VStack {
+                    Text("New around here?")
+                    
+                    NavigationLink("Create An Account", destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+            }
+            Spacer()
+            
+            }
+        }
+    
 #Preview {
     LoginView()
 }
+    
